d9e78ef0160c406fb7ee4be8024cc6acea29507e
Noorkhokhar <34125851+noorkhokhar99@users.noreply.github.com>
2019-12-31 17:33:18 +0500

    Add files via upload

---
+++ sht20.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+#include "DFRobot_SHT20.h"
+
+DFRobot_SHT20    sht20;
+
+void setup()
+{
+  Serial.begin(9600);
+    sht20.initSHT20();                                  // Init SHT20 Sensor
+    delay(100);
+    sht20.checkSHT20();       
+    Serial.println("check");// Check SHT20 Sensor
+}
+
+void loop()
+{
+    float humd = sht20.readHumidity();                  // Read Humidity
+    float temp = sht20.readTemperature();               // Read Temperature
+  //  Serial.print("Time:");
+  //  Serial.print(millis());
+    Serial.print(" Temperature:");
+    Serial.print(temp, 1);
+    Serial.print("C");
+    Serial.print(" Humidity:");
+    Serial.print(humd, 1);
+    Serial.print("%");
+    Serial.println();  
+    delay(2000);
+}
